290b732daee7608d8dbe54dfe7921fa06c51de00
diixo <anatolii.podoba@gmail.com>
2025-10-11 03:47:52 +0300

    ex3.cmd

--- cmds/ex3.cmd
+++ cmds/ex3.cmd
@@ -7,19 +7,19 @@ goto :MAIN
 
 :: --------------------
 :FuncA
-echo [FuncA] arg: %~1
+echo [FuncA] arg: %~1, arg: %~2
 exit /b
 
 :: --------------------
 :FuncB
-echo [FuncB] arg: %~1
+echo [FuncB] arg: %~1, arg: %~2
 exit /b
 
 :: -------- Entry point
 :MAIN
 echo === start MAIN ===
 
-call :FuncA Hello
-call :FuncB World
+call :FuncA Hello1 world1
+call :FuncB Hello2 world2
 
 echo === finish MAIN ===
